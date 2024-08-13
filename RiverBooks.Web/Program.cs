c699905d2fbc5dfab1eb34f3e96611f4ddecb3d2
Yusuf Çırak <yusuf10901@hotmail.com>
2024-08-14 00:19:52 +0300

    initial

---
+++ RiverBooks.Web/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using RiverBooks.Books;
+using RiverBooks.Users;
+using Serilog;
+
+var logger = Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
+logger.Information("Starting web host");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, config) => 
+  config.ReadFrom.Configuration(builder.Configuration));
+
+builder.Services.AddFastEndpoints()
+  .AddJWTBearerAuth(builder.Configuration["Auth:JwtSecret"]!)
+  .AddAuthorization()
+  .SwaggerDocument();
+
+// Add Module Services
+List<Assembly> mediatRAssemblies = [typeof(Program).Assembly];
+builder.Services.AddBookServices(builder.Configuration, logger, mediatRAssemblies);
+builder.Services.AddUserModuleServices(builder.Configuration, logger, mediatRAssemblies);
+
+// Set up MediatR
+builder.Services.AddMediatR(cfg =>
+  cfg.RegisterServicesFromAssemblies(mediatRAssemblies.ToArray()));
+
+
+var app = builder.Build();
+
+app.UseAuthentication()
+  .UseAuthorization();
+
+app.UseFastEndpoints()
+  .UseSwaggerGen();
+
+app.Run();
+
+public partial class Program { } // needed for tests
